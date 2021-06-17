64171c0671c07b512624c4025a461de61f5ce71a
Yonis Hassan <hyonis@fb.com>
2021-06-17 18:27:00 -0500

    fix rest btn

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,15 @@
 console.log("app.js connected");
 
-//
 
 function buttonClick() {
-    d3.text('/magic').then (function(d){
+  d3.text('/magic').then (function(d){
     // console.log(d)
     document.getElementById("prediction").textContent = 'The Modle Predicts' + d
-    
-    
-    function clearButton() {
-      var pTag = (document.getElementById('prediction').innerHTML = '');
-    
-    }
   });
 }
+
+function clearButton() {
+  var pTag = (document.getElementById('prediction').innerHTML = '');
+}
+
+
